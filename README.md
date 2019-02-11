d3fde763af8443d0abfdd55153a3cd0c5e001188
Chris Last <chris.last@gmail.com>
2019-02-11 19:19:42 +0000

    2

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # whatsup
 Inky WHAT info console for Raspberry Pi
+
+# py -m venv venv371
+# venv371\Scripts\activate
+# pip install pandas
+# pip install bs4
+# pip install lxml
+# pip install einky
+
+# crontab
+minute(0-59) hour(0-23) day(1-31) month(1-12) weekday(0-6) command
+
+# schedule
+# update buses every 10 minutes during the day
+0,10,20,30,40,50 7-19 * * * buses.py
+# update calendar every day @ midnight
+0 0 * * * calendar.py
+# update headlines every hour
+0 * * * * headlines.py
+# update bins every day @ 4am
+0 4 * * * bins.py
+
+
+
+
+# ☀☁☂☃☄★☆☇☈☉☊☋☌☍☎☏☐☑☒☓☔☕☖☗☘☙☚☛☜☝☞☟
+# ☠☡☢☣☤☥☦☧☨
+# ☩☪☫☬☭☮☯
+# ☰☱☲☳☴☵☶☷
+# ☸☹☺☻☼☽☾☿♀♁♂♃♄♅♆♇
+# ♈♉♊♋♌♍♎♏♐♑♒♓
+# ♔♕♖♗♘♙♚♛♜♝♞♟♠♡♢♣♤♥♦♧♨♩♪♫♬♭♮♯♰♱
+# ♲♳♴♵♶♷♸♹♺♻♼♽
+# ♾♿⚀⚁⚂⚃⚄⚅⚆⚇⚈⚉⚊⚋⚌⚍⚎⚏⚐⚑⚒⚓⚔⚕⚖⚗⚘⚙⚚⚛⚜⚝⚞⚟⚠
+# ⚡⚢⚣⚤⚥
+# ⚦⚧⚨⚩⚪⚫⚬⚭⚮⚯⚰⚱⚲⚳⚴⚵⚶⚷⚸⚹⚺⚻⚼⚽⚾⚿⛀⛁⛂⛃⛄
+# ⛅⛆⛇⛈⛉⛊⛋⛌⛍⛎⛏
+# ⛐⛑⛒⛓⛔⛕⛖⛗⛘⛙⛚⛛⛜⛝⛞⛟⛠⛡⛢⛣⛤⛥⛦⛧⛨⛩⛪⛫⛬⛭⛮⛯⛰⛱⛲⛳
+# ⛴⛵⛶⛷⛸⛹⛺⛻⛼⛽⛾⛿✀✁✂✃✄✅✆✇✈
--- whatsup.py
+++ whatsup.py
@@ -1,111 +1,23 @@
-# py -m venv venv371
-# venv371\Scripts\activate
-# pip install pandas
-# pip install bs4
-# pip install lxml
+"""Display useful info on an e-ink display"""
+from inky import InkyWHAT
 
-import pandas as pd
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from bus_info import update_buses
+from cal_info import update_calendar
+from bin_info import update_bins
+from news_info import update_headlines
 
-import pdb
-pdb.set_trace()
+inkywhat = InkyWHAT('red')
 
-#url = "http://www.hubertiming.com/results/2017GPTR10K"
-url = "https://bustimes.org/stops/0500CCITY484"
-url = "https://bustimes.org/stops/0500SCAMB024"
-html = urlopen(url)
+def update_background():
+    pass
 
-soup = BeautifulSoup(html, 'lxml')
-type(soup)
+def main():
+    """."""
+    update_background()
+    update_buses()
+    update_calendar()
+    update_bins()
+    update_headlines()
 
-# Get the title
-title = soup.title
-print(title)
-
-# Print out the text
-text = soup.get_text()
-#print(soup.text)
-
-soup.find_all('a')
-
-all_links = soup.find_all("a")
-for link in all_links:
-    print(link.get("href"))
-
-
-# Print the first 10 rows for sanity check
-rows = soup.find_all('tr')
-print(rows[:10])
-
-for row in rows:
-    row_td = row.find_all('td')
-print(row_td)
-type(row_td)
-
-
-str_cells = str(row_td)
-cleantext = BeautifulSoup(str_cells, "lxml").get_text()
-print(cleantext)
-
-import re
-
-list_rows = []
-for row in rows:
-    cells = row.find_all('td')
-    str_cells = str(cells)
-    clean = re.compile('<.*?>')
-    clean2 = (re.sub(clean, '',str_cells))
-    list_rows.append(clean2)
-print(clean2)
-type(clean2)
-
-# list_rows[0] = '[\n4\n, Cambourne, \n13:15⚡\n]'
-
-df = pd.DataFrame(list_rows)
-df.head(10)
-
-df1 = df[0].str.split(',', expand=True)
-df1.head(10)
-
-col_labels = soup.find_all('th')
-
-all_header = []
-col_str = str(col_labels)
-cleantext2 = BeautifulSoup(col_str, "lxml").get_text()
-all_header.append(cleantext2)
-print(all_header)
-
-df2 = pd.DataFrame(all_header)
-df2.head()
-
-df3 = df2[0].str.split(',', expand=True)
-df3.head()
-
-frames = [df3, df1]
-
-df4 = pd.concat(frames)
-df4.head(10)
-
-# Below shows how to assign the first row to be the table header
-df5 = df4.rename(columns=df4.iloc[0])
-df5.head()
-
-
-df5.info()
-df5.shape
-
-# ☀☁☂☃☄★☆☇☈☉☊☋☌☍☎☏☐☑☒☓☔☕☖☗☘☙☚☛☜☝☞☟
-# ☠☡☢☣☤☥☦☧☨
-# ☩☪☫☬☭☮☯
-# ☰☱☲☳☴☵☶☷
-# ☸☹☺☻☼☽☾☿♀♁♂♃♄♅♆♇
-# ♈♉♊♋♌♍♎♏♐♑♒♓
-# ♔♕♖♗♘♙♚♛♜♝♞♟♠♡♢♣♤♥♦♧♨♩♪♫♬♭♮♯♰♱
-# ♲♳♴♵♶♷♸♹♺♻♼♽
-# ♾♿⚀⚁⚂⚃⚄⚅⚆⚇⚈⚉⚊⚋⚌⚍⚎⚏⚐⚑⚒⚓⚔⚕⚖⚗⚘⚙⚚⚛⚜⚝⚞⚟⚠
-# ⚡⚢⚣⚤⚥
-# ⚦⚧⚨⚩⚪⚫⚬⚭⚮⚯⚰⚱⚲⚳⚴⚵⚶⚷⚸⚹⚺⚻⚼⚽⚾⚿⛀⛁⛂⛃⛄
-# ⛅⛆⛇⛈⛉⛊⛋⛌⛍⛎⛏
-# ⛐⛑⛒⛓⛔⛕⛖⛗⛘⛙⛚⛛⛜⛝⛞⛟⛠⛡⛢⛣⛤⛥⛦⛧⛨⛩⛪⛫⛬⛭⛮⛯⛰⛱⛲⛳
-# ⛴⛵⛶⛷⛸⛹⛺⛻⛼⛽⛾⛿✀✁✂✃✄✅✆✇✈
+if __name__ == "__main__":
+    main()
