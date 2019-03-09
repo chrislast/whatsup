dd15f76fc33fff88b5a5ac89e59b3aafc9e06e97
Chris Last <chris.last@gmail.com>
2019-03-09 11:13:58 +0000

    just tidy ups needed now and map unseen weather strings to icons

--- whatsup.py
+++ whatsup.py
@@ -5,6 +5,7 @@ import re
 import json
 from urllib.request import urlopen
 import datetime
+from collections import namedtuple
 
 # External packages
 import dateutil.parser
@@ -30,6 +31,65 @@ MON = {
     1:"Jan", 2:"Feb", 3:"Mar", 4:"Apr", 5:"May", 6:"Jun",
     7:"Jul", 8:"Aug", 9:"Sep", 10:"Oct", 11:"Nov", 12:"Dec"}
 
+# Weather icon positions in weather.png
+WeatherIcon=namedtuple("WeatherIcon","x1 y1 x2 y2")
+# Row 1
+SUNNY_DAY     = WeatherIcon(9, 0, 53, 43)
+CLEAR_NIGHT   = WeatherIcon(82, 7, 111, 37)
+LIGHT_CLOUD   = WeatherIcon(138, 7, 186, 37)
+HEAVY_CLOUD   = WeatherIcon(200, 6, 260, 42)
+WIND_CLOUD    = WeatherIcon(275, 7, 330, 44)
+STRONG_WIND   = WeatherIcon(346, 10, 396, 40)
+# Row 2
+SHOWERS       = WeatherIcon(7, 51, 55, 91)
+LIGHT_RAIN    = WeatherIcon(73, 51, 121, 95)
+RAINY         = WeatherIcon(138, 51, 186, 95)
+HEAVY_RAIN    = WeatherIcon(206, 51, 254, 95)
+THUNDER       = WeatherIcon(278, 51, 326, 94)
+HEAVY_THUNDER = WeatherIcon(347, 51, 394, 94)
+# Row 3
+PARTLY_CLOUDY_DAY   = WeatherIcon(0, 102, 62, 145)
+PARTLY_CLOUDY_NIGHT = WeatherIcon(72, 107, 122, 145)
+SNOW                = WeatherIcon(138, 105, 186, 145)
+SLEET               = WeatherIcon(206, 104, 254, 147)
+PARTLY_SNOWY_DAY    = WeatherIcon(268, 100, 331, 153)
+PARTLY_SNOWY_NIGHT  = WeatherIcon(345, 100, 396, 149)
+# Row 4
+PARTLY_SHOWERS_DAY   = WeatherIcon(0, 161, 63, 215)
+PARTLY_SHOWERS_NIGHT = WeatherIcon(70, 166, 121, 215)
+PARTLY_RAIN_DAY      = WeatherIcon(130, 160, 194, 218)
+PARTLY_RAIN_NIGHT    = WeatherIcon(203, 166, 254, 218)
+PARTLY_WINDY_DAY     = WeatherIcon(265, 161, 335, 212)
+PARTLY_WINDY_NIGHT   = WeatherIcon(340, 166, 398, 212)
+# Row 5
+UNKNOWN = WeatherIcon(360, 231, 377, 260)
+
+# Map weather descriptions to weather icons
+# first entry is daytime, last entry is night time
+WEATHER_ICONS={
+    "Sunny" : [SUNNY_DAY],
+    "Clear Sky" : [SUNNY_DAY, CLEAR_NIGHT],
+
+    "Sunny Intervals" : [PARTLY_CLOUDY_DAY],
+    "Partly Cloudy" : [PARTLY_CLOUDY_DAY, PARTLY_CLOUDY_NIGHT],
+    "Light Cloud" : [LIGHT_CLOUD],
+    "Heavy Cloud" : [HEAVY_CLOUD],
+
+    "Light Rain Showers" : [PARTLY_SHOWERS_DAY, PARTLY_SHOWERS_NIGHT],
+    "Light Rain" : [LIGHT_RAIN],
+    "Heavy Rain" : [HEAVY_RAIN],
+
+    "Light Snow Showers" : [PARTLY_SNOWY_DAY, PARTLY_SNOWY_NIGHT],
+    "Snow Showers" : [SNOW],
+    "Sleet": [SLEET],
+
+    "Thunderstorms" : [THUNDER],
+    "Heavy Thunderstorms" : [HEAVY_THUNDER],
+
+    "Light Breeze" : [PARTLY_WINDY_DAY, PARTLY_WINDY_NIGHT],
+    "Strong Winds" : [STRONG_WIND],
+}
+
 TODAY = dateutil.utils.today()
 NOW = datetime.datetime.now()
 
@@ -38,7 +98,7 @@ def get_background(background_img=BACKGROUND):
     _img = Image.open(background_img)
 
     # Check the width and height of the image
-    assert img.size == (400, 300), "Background must be 400x300"
+    assert _img.size == (400, 300), "Background must be 400x300"
 
     # Convert the image to use a white / black / red colour palette
     # hopefully it already did
@@ -51,8 +111,10 @@ def get_background(background_img=BACKGROUND):
 def show_text(txt, x, y, alignment="left", colour=InkyWHAT.BLACK, font=HankenGroteskBold, font_size=16):
     """."""
     # Calculate the positioning and draw the text
+    print(txt)
     font = ImageFont.truetype(font, font_size)
     text_w, text_h = font.getsize(txt)
+    draw = ImageDraw.Draw(img)
     if alignment == "left":
         draw.text((x, y), txt, colour, font=font)
     if alignment == "right":
@@ -66,10 +128,13 @@ def update_buses():
     soup = BeautifulSoup(html, "html.parser")
 
     number_of_buses = 4
-    for row, text in enumerate(soup.find_all('tr')[:number_of_buses]):
+    rows = soup.find_all('tr')[:number_of_buses]
+    if not rows:
+        show_text("No bus timetable", 10, 135)
+    for row, text in enumerate(rows):
         bus = ' '.join(text.stripped_strings)
         bus = re.sub("⚡", '*', bus)
-        show_text(img, bus, 10, 100 + 15 * row)
+        show_text(bus, 10, 100 + 15 * row)
 
 def update_bins():
     """."""
@@ -117,24 +182,36 @@ def update_weather():
     data = json.loads(html.read().decode())
     forecasts = data["forecasts"]
     # location = data["location"]
+    is_night = data["isNight"]
 
-    # show todays detailed report
+    # show current detailed report
+    now_report = forecasts[0]["detailed"]["reports"][0]
     summary_report = forecasts[0]["summary"]["report"] # 'pollenIndex', 'pollenIndexText', 'windDirectionFull', 'gustSpeedMph', 'sunset', 'lowermaxTemperatureF', 'windSpeedMph', 'weatherType', 'pollenIndexIconText', 'mostLikelyLowTemperatureF', 'precipitationProbabilityText', 'precipitationProbabilityInPercent', 'uppermaxTemperatureF', 'lowerminTemperatureF', 'uvIndex', 'uvIndexBand', 'pollutionIndex', 'mostLikelyLowTemperatureC', 'lowerminTemperatureC', 'windDescription', 'localDate', 'pollutionIndexBand', 'maxTempF', 'pollutionIndexIconText', 'upperminTemperatureF', 'upperminTemperatureC', 'windSpeedKph', 'uvIndexText', 'sunrise', 'mostLikelyHighTemperatureC', 'maxTempC', 'pollutionIndexText', 'minTempF', 'minTempC', 'windDirection', 'uvIndexIconText', 'mostLikelyHighTemperatureF', 'lowermaxTemperatureC', 'gustSpeedKph', 'weatherTypeText', 'pollenIndexBand', 'enhancedWeatherDescription', 'uppermaxTemperatureC', 'windDirectionAbbreviation'
-    _wd = summary_report["windDirectionAbbreviation"] # 'S'
-    _ws = summary_report["windSpeedMph"] # int
+    _wd = now_report["windDirectionAbbreviation"] # 'S'
+    _ws = now_report["windSpeedMph"] # int
     _sr = summary_report["sunrise"] # '6:56'
     _ss = summary_report["sunset"] # '17:32'
+    _te = now_report["temperatureC"] # int
     _mn = summary_report["minTempC"] # int
     _mx = summary_report["maxTempC"] # int
-    _wt = summary_report["weatherTypeText"] # str
-    _pp = summary_report["precipitationProbabilityInPercent"] # int
-    weather = Image.open("sunny.png", "r")
-    img.paste(weather, (157, 99))
-    show_text("Temp: %d-%d°C" % (_mn, _mx), 208, 157, font_size=14, alignment="centre")
+    _wt = now_report["weatherTypeText"] # str
+
+    # Show the weather icon
+    weather = Image.open("weather.png", "r")
+    if is_night:
+        icon_xy = WEATHER_ICONS.get(_wt, UNKNOWN)[-1]
+    else:
+        icon_xy = WEATHER_ICONS.get(_wt, UNKNOWN)[0]
+    weather = weather.crop(icon_xy)
+    centred = 157 + 50 - (icon_xy.x2-icon_xy.x1) // 2
+    img.paste(weather, (centred, 100)) # 157-257, 100-156
+
+
+    show_text("Temp: %d°C" % (_te), 208, 157, font_size=14, alignment="centre")
     show_text("Sunrise: %s" % _sr, 208, 171, font_size=14, alignment="centre")
     show_text("Sunset: %s" % _ss, 208, 185, font_size=14, alignment="centre")
-    show_text("Wind: %s %dmph" % (_wd, _ws), 208, 199, font_size=14, alignment="centre")
-    show_text(_wt, 208, 213, alignment="centre")
+    show_text("Wind: %s %dmph" % (_wd, _ws), 208, 199, font_size=12, alignment="centre")
+    show_text(_wt, 208, 213, font_size=14, alignment="centre")
 
     # Show 3 day summary
     for day in (1, 2, 3):
@@ -158,14 +235,16 @@ def update_weather():
         ppp = summary_report["precipitationProbabilityInPercent"]
         bar_height = (100 - ppp) // 3
         bar = [origin[0] + hoffset, origin[1], origin[2] + hoffset, origin[3] + bar_height]
+        draw = ImageDraw.Draw(img)
         draw.rectangle(bar, fill=InkyWHAT.WHITE)
 
     # Write the days of the week on X axis of rainfall graph
     wkday = TODAY.weekday()
-    wkimg = Image.open("fortnight.png") # 20 * 6px x 7px
+    wkimg = Image.open("fortnight2.png") # 20 * 6px x 7px
     # Crop the correct 14 days from the 20 in the image
     wkimg = wkimg.crop((wkday*6, 0, (wkday+14)*6, 8))
     img.paste(wkimg, (315, 223))
+
 
 
 def update_birthdays():
@@ -186,7 +265,7 @@ def main():
     update_birthdays()
 
     # Display the final image on Inky wHAT
-    inky_display.set_image()
+    inky_display.set_image(img)
     inky_display.show()
 
 if __name__ == "__main__":
@@ -195,6 +274,5 @@ if __name__ == "__main__":
     inky_display.set_border(inky_display.BLACK)
 
     img = get_background()
-    draw = ImageDraw.Draw(img)
 
     main()
